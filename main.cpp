f7e8f77b35d16cbca352f4d77c3b716adee22889
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2022-02-22 14:43:17 +0800

    show the plugin path for debug

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <QApplication>
+#include <QtCore/qlibraryinfo.h>
 
 #include "mainwindow.h"
 
 int main(int argc, char* argv[]) {
+    qDebug() << QLibraryInfo::location(QLibraryInfo::PluginsPath);
+
     QApplication app(argc, argv);
     MainWindow window;
     window.setWindowTitle("ImageViewer");
